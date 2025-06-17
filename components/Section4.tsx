164d81d9a8bb871d539d0ed592f853fa3ffab163
Vadim Borodich <vborodich@VADIMS-MACBOOK-PRO.local>
2025-06-17 18:27:32 +0300

    Added fixes

--- components/Section4.tsx
+++ components/Section4.tsx
@@ -1,14 +1,14 @@
 const Section4 = () => {
     return (
         <>
-          <section className="u-clearfix u-valign-bottom u-section-4">
-              <div className="u-clearfix u-expanded-width u-gutter-20 u-layout-wrap-1">
+          <section className="u-clearfix u-valign-bottom u-valign-bottom-xl u-section-4">
+              <div className="data-layout-selected u-clearfix u-expanded-width u-gutter-20 u-layout-wrap u-layout-wrap-1">
                   <div className="u-layout">
                       <div className="u-layout-row">
                           <div className="u-container-align-center u-container-style u-image u-layout-cell u-size-30 u-image-4">
                           </div>
-                          <div className="u-container-align-center u-container-style u-grey-5 u-layout-cell u-size-30"
-                               data-animation-name="customAnimationIn" data-animation-duration="2000" data-animation-delay="0">
+                          <div
+                              className="u-container-align-center-md u-container-align-center-sm u-container-align-center-xs u-container-style u-grey-5 u-layout-cell u-size-30">
                               <div
                                   className="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xs">
                                   <h2 className="u-align-center u-text u-text-default u-text-1"> Принудительный возврат займа</h2>
